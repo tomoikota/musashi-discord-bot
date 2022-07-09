6a7ae8546139b5026ba76cb26bb8f08e7616afa3
tomoikota <108983934+tomoikota@users.noreply.github.com>
2022-07-09 21:37:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,47 @@
-from discord.ext import commands
-from os import getenv
-import traceback
+import discord
 
-bot = commands.Bot(command_prefix='/')
+token = getenv('BOT_TOKEN')
+client = discord.Client()
 
+#リアクション関係のメソッド
+@client.event
+async def on_raw_reaction_add(payload):
+    
+    #指定したメッセージにリアクションがついたら。
+    if payload.message_id == 990286766663671879: 
+        #サーバーの情報を取得
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+        #サーバー情報からロール情報を取得
+        role = guild.get_role(990282640210931772)
+        #取得したロール情報をリアクションしたユーザに付与。IDがあっていればこれでリアクションロールはOK。
+        await payload.member.add_roles(role)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#メッセージ関係のメソッド
+@client.event
+async def on_message(message):
+    if message.content == "/ping":
+        # Ping値を取得（変数名に用いている「raw」は「未加工」等の意味を持つ。
+        # discord.pyの速度取得は秒単位で取得するため単位を変換する必要がある。）
+        raw_ping = client.latency
 
+        # Pingの単位はms（ミリ秒）を用いる。ミリ秒に変換する。
+        ping = round(raw_ping * 1000)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+        # pingの数値によってメッセージを変更する。
+        # 判定基準はここを参照した。https://tinyurl.com/2orgg533
+        if ping <= 15:
+            judge = '非常に速いです。'
+        elif ping <= 35:
+            judge = '速いです。'
+        elif ping <= 50:
+            judge = '普通です。'
+        elif ping <= 100:
+            judge = '遅いです。'
+        else:
+            judge = '非常に遅いです。'
 
+        #メッセージを出力する。pythonコマンド上だとbotが遅いのかPing値が大きい。
+        await message.reply(f"Ping値は{ping}msです。\n{judge}")
 
-token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
