ac6e69bdae65475d1c93151a4d6f06bc191ab3e9
tomoikota <108983934+tomoikota@users.noreply.github.com>
2022-07-10 10:27:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,6 @@ import discord
 import os
 
 token = os.environ['DISCORD_BOT_TOKEN']
-messageId = os.environ['MESSAGE_ID']
-roleId = os.environ['ROLE_ID']
 client = discord.Client()
 
 
@@ -12,12 +10,12 @@ client = discord.Client()
 async def on_raw_reaction_add(payload):
 
     # 指定したメッセージにリアクションがついたら。
-    if payload.message_id == messageId:
+    if payload.message_id == 990286766663671879:
         # サーバーの情報を取得
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
         # サーバー情報からロール情報を取得
-        role = guild.get_role(roleId)
+        role = guild.get_role(990282640210931772)
         # 取得したロール情報をリアクションしたユーザに付与。IDがあっていればこれでリアクションロールはOK。
         await payload.member.add_roles(role)
 
